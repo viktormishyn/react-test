7fc672fda2caacb76192db056b9ba62212445f24
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-28 14:44:01 +0300

    Use component <App />

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <ul>
-    <li>Monday</li>
-    <li>Tuesday</li>
-    <li>Wednesday</li>
-  </ul>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
 
