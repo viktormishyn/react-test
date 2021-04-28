53364c678bc0ec846b3b0e385d5e545260b716e1
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-28 16:20:59 +0300

    Array in props

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -15,6 +15,9 @@ function Main(props) {
       <p>
         We serve the most {props.adjective} food around.
       </p>
+      <ul>
+        {props.dishes.map(dish => <li>{dish}</li>)}
+      </ul>
     </section>
   )
 }
@@ -27,11 +30,18 @@ function Footer(props) {
   )
 }
 
+const dishes = [
+  "Macaroni and Cheese",
+  "Salmon",
+  "Tofu with Vegetables"
+]
+
+
 function App() {
   return (
     <div className="App">
       <Header name="John" />
-      <Main adjective="amazing" />
+      <Main adjective="amazing" dishes={dishes} />
       <Footer year={new Date().getFullYear()} />
     </div>
   );
