8974b5a7fc56c11f1ca3e979d3de4fac71e38135
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-28 14:13:48 +0300

    React.createElement nested

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  React.createElement("h1", {style: {color: "blue"}}, "Hello world"),
+  React.createElement(
+    "ul",
+    null,
+    React.createElement("li", null, "Monday"),
+    React.createElement("li", null, "Tuesday"),
+    React.createElement("li", null, "Thursday")
+  ),
   document.getElementById('root')
 );
 
