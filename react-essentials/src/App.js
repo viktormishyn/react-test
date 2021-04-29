7f75877a3211b6e9033b99f1c6c7c42072c404f0
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-29 11:58:08 +0300

    Multiple states (useState) and side effects (useEffect)

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -5,18 +5,26 @@ function App() {
   const [emotion, setEmotion] = useState("happy")
   // useState returns an array, where first item (corresponds to the first argument) is our state variable
   // and second is a function for updating the state
+  const [secondary, setSecondary] = useState("tired")
 
   useEffect( () => {
     console.log(`It's ${emotion} around here!`)
   }, [emotion])
   // useEffect takes 2 arguments: useState is going to watch the state values from the dependency array (2nd argument)
   // if the value changes, useEffect will call the sideffect function (1st argument)
+  useEffect( () => {
+    console.log(`It's ${secondary} around here!`)
+  }, [secondary])
+
 
   return (
     <>
-      <h1>Current emotion: {emotion}</h1>
+      <h1>Current emotions: {emotion}, {secondary}</h1>
       <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
       <button onClick={() => setEmotion("happy")}>Happy</button>
+      <br/>
+      <button onClick={() => setSecondary("crabby")}>Crabby</button>
+
     </>
   )
 }
