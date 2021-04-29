134d044d700e3bdeb23f2e63cbca27071e06fc34
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-29 10:01:17 +0300

    Array destructuring

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const [,, thirdItem] = ["boots", "tent", "headlamp"]
+console.log(thirdItem)
 
 ReactDOM.render(
   <App authorized={false} />,
