6c9277bc77f371beca88b4a0160bfb63ce23d710
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-29 14:37:16 +0300

    Handling loading states

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -5,22 +5,29 @@ import { useState, useEffect } from "react"
 
 function App({ login }) {
   const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
+    if (!login) return
+    setLoading(true)
     fetch(`https://api.github.com/users/${login}`)
       .then(response => response.json())
       .then(setData)
-  }, [])
+      .then(() => setLoading(false))
+      .catch(setError)
+  }, [login])
 
-  if (data) {
-    return (<div>
-      <h1>{data.name}</h1>
-      <p>{data.location}</p>
-      <img src={data.avatar_url} alt={data.login}/>
-    </div>)
-  }
+  if (loading) return <h1>Loading...</h1>
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+  if (!data) return null
 
-  return <div>No User Available</div>
+
+  return (<div>
+    <h1>{data.name}</h1>
+    <p>{data.location}</p>
+    <img src={data.avatar_url} alt={data.login} />
+  </div>)
 }
 
 export default App;
