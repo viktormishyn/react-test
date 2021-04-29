4af1a7785af1eb18e1662ff902101de17f963535
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-29 11:45:24 +0300

    sideEffect hook

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function App() {
   const [emotion, setEmotion] = useState("happy")
   // useState returns an array, where first item (corresponds to the first argument) is our state variable
   // and second is a function for updating the state
-  console.log(emotion)
+
+  useEffect( () => {
+    console.log(`It's ${emotion} around here!`)
+  }, [emotion])
+  // useEffect takes 2 arguments: useState is going to watch the state values from the dependency array (2nd argument)
+  // if the value changes, useEffect will call the sideffect function (1st argument)
+
   return (
     <>
       <h1>Current emotion: {emotion}</h1>
