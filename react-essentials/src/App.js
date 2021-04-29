b6f3e0d155014fd845c37a5e172b04922c64eecf
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-29 13:29:58 +0300

    Fetch data with useState and useEffect hooks

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,25 +1,22 @@
 import './App.css';
-import { useReducer } from "react"
+import { useState, useEffect } from "react"
 
-function App() {
-  const [checked, toggle] = useReducer(
-    (checked) => !checked,
-    false
-  )
-  // reducer takes in the current state and returns a new state.
-  // we can use useReducer instead of hardcoding such behavior intho onChange events
-  // first argument in useReducer hook - reducer function, second - initial state
+// https://api.github.com/users/viktormishyn
 
-  return (
-    <>
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={toggle}
-      />
-      <p>{checked ? "checked" : "not checked"}</p>
-    </>
-  )
+function App({ login }) {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then(response => response.json())
+      .then(setData)
+  }, [])
+
+  if (data) {
+    return <div>{JSON.stringify(data)}</div>
+  }
+
+  return <div>No User Available</div>
 }
 
 export default App;
