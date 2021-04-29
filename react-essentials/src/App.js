37f37c985bc1a03a75930f38781320c9fd9ae5b7
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-29 20:09:30 +0300

    Creating links

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import './App.css';
 import { Routes, Route } from "react-router-dom"
-import { Home, About, Events, Contact } from "./pages"
+import { Home, About, Events, Contact, Whoops404 } from "./pages"
 
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
       <Route path="/about" element={<About />} />
       <Route path="/events" element={<Events />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<Whoops404 />} />
       </Routes>
     </div>
   )
--- react-essentials/src/pages.js
+++ react-essentials/src/pages.js
@@ -1,9 +1,15 @@
 import React from "react"
+import { Link, useLocation } from "react-router-dom"
 
 export function Home() {
     return (
         <div>
             <h1>[Company Site]</h1>
+            <nav>
+                <Link to="about">About</Link>
+                <Link to="events">Events</Link>
+                <Link to="contact">Contact</Link>
+            </nav>
         </div>
     )
 }
@@ -28,6 +34,16 @@ export function Contact() {
     return (
         <div>
             <h1>[Contact]</h1>
+        </div>
+    )
+}
+
+export function Whoops404() {
+    let location = useLocation()
+    console.log(location)
+    return (
+        <div>
+            <h1>Resource not found at {location.pathname}</h1>
         </div>
     )
 }
