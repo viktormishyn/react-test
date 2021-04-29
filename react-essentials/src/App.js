089a0893a406a21817b8e37b3628c1262879ffa3
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-29 18:11:25 +0300

    React testing library

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,33 +1,13 @@
 import './App.css';
-import { useState, useEffect } from "react"
-
-// https://api.github.com/users/viktormishyn
-
-function App({ login }) {
-  const [data, setData] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
-
-  useEffect(() => {
-    if (!login) return
-    setLoading(true)
-    fetch(`https://api.github.com/users/${login}`)
-      .then(response => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError)
-  }, [login])
-
-  if (loading) return <h1>Loading...</h1>
-  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>
-  if (!data) return null
 
 
-  return (<div>
-    <h1>{data.name}</h1>
-    <p>{data.location}</p>
-    <img src={data.avatar_url} alt={data.login} />
-  </div>)
+function App() {
+
+  return (
+    <div>
+      <h1>Hello React Testing Library</h1>
+    </div>
+  )
 }
 
 export default App;
