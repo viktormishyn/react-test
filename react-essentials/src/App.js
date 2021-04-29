f5b9aaead0c50b447d1747aec155dd4a3e72d4a9
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-29 19:55:53 +0300

    Create routes

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,11 +1,19 @@
+import React from "react"
 import './App.css';
+import { Routes, Route } from "react-router-dom"
+import { Home, About, Events, Contact } from "./pages"
 
 
 function App() {
 
   return (
     <div>
-      <h1>Hello React Testing Library</h1>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   )
 }
